e0dc840602f51f27fc210ce89c0446f3326df707
Evan Turner <evturn@gmail.com>
2015-12-04 14:30:24 -0500

    Add compile prepublish script

---
+++ src/proto.js
@@ -0,0 +1,55 @@
+'use strict';
+const getOwnPropNames = Object.getOwnPropertyNames;
+const getOwnPropDescriptor = Object.getOwnPropertyDescriptor;
+const hasOwnProp = hasOwnProperty;
+const defineProp = Object.defineProperty;
+
+exports = module.exports = {
+
+  /**
+   * Instance - "this" is the prototype of the new instance
+   *
+   */
+
+  new: function() {
+    const instance = Object.create(this);
+
+    if (instance.constructor) {
+      instance.constructor.apply(instance, arguments);
+    }
+    return instance;
+  },
+
+  /**
+   * Subclass - "this" is a prototype object used as a class
+   *
+   */
+
+  extend: function(props) {
+    const descriptor = {};
+
+    getOwnPropNames(props).forEach((name) => {
+      descriptor[name] = getOwnPropDescriptor(props, name)
+    });
+
+    const subProto = Object.create(this, descriptor)
+
+    subProto.super = this;
+    return subProto;
+  },
+
+    /**
+   * Add properties from other objects
+   *
+   */
+
+  mixin: function(target, source) {
+    getOwnPropNames(src).forEach((name) => {
+      if (hasOwnProp.call(target, name)) { return; }
+      const descriptor = getOwnPropDescriptor(source, name);
+
+      defineProp(target, name, descriptor);
+    });
+    return target;
+  }
+};
